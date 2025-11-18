ee62f03991862daadd05aba85883dee368c0a96b
MilkyWay337 <milky300way@gmail.com>
2025-11-19 00:54:11 +0300

    Update __init__.py

--- pymc_bart/__init__.py
+++ pymc_bart/__init__.py
@@ -15,7 +15,7 @@ import pymc as pm
 
 from pymc_bart.bart import BART
 from pymc_bart.pgbart import PGBART
-from pymc_bart.split_rules import ContinuousSplitRule, OneHotSplitRule, SubsetSplitRule, TargetMeanSplitRule
+from pymc_bart.split_rules import ContinuousSplitRule, OneHotSplitRule, SubsetSplitRule, TargetMeanSplitRule  # Добавлен TargetMeanSplitRule
 from pymc_bart.utils import (
     compute_variable_importance,
     get_variable_inclusion,
@@ -34,7 +34,7 @@ __all__ = [
     "ContinuousSplitRule",
     "OneHotSplitRule",
     "SubsetSplitRule",
-    "TargetMeanSplitRule",
+    "TargetMeanSplitRule",  # Добавлен
     "compute_variable_importance",
     "get_variable_inclusion",
     "plot_convergence",
